3c9e30caf8f265d51ee429ca41d6d9100a4dd8f7
AngieMonroe <ame.monroy@gmail.com>
2018-09-27 17:29:17 -0500

    Mostrando posts en el muro

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -5,59 +5,56 @@ import firebaseConf from '../config/firebaseConf';
 class PostList extends Component {
      constructor(props){
          super(props);
-         this.database = firebaseConf.database().ref().child('postReact'); 
+         this.database = firebaseConf.database().ref('postReact'); 
          this.deletePost = this.deletePost.bind(this);
-         this.editPost = this.editPost.bind(this);
-         this.likePost = this.likePost.bind(this);
-         this.dislikePost = this.dislikePost.bind(this);   
+         this.editPost = this.editPost.bind(this);  
          this.state = {
-             posts: []
-         }
+            posts : [],
+         };
      }
 
      componentDidMount(){
-         this.database.on('value', snap => {
-             this.setState({
-                 posts: snap.val()
-             })
+         this.database.on('value', snapshot => {             
+            const posts = [];
+            snapshot.forEach(element => {
+                const dataPost = element.val()
+                posts.push(dataPost);
+            })
+            this.setState({ posts: posts});
          })
      }
 
-     deletePost (){
-
+     deletePost(keyPost) {
+         if (window.    confirm('Quieres eliminar este post')){
+         this.database.ref('postReact').child(keyPost).remove();
+         } else {
+             return false;
+         }
      }
 
      editPost() {
 
      }
 
-     likePost() {
-
-     }
-
-     dislikePost() {
-
-     }
 
     render() {
         console.log(this.state.posts)
-        console.log(Object.keys(this.state.posts))
-        const { post } = this.state;
-
         return (
             <div className="container">
-                <Card className="mt-3 col-sm-1 col-md-9">
+            {this.state.posts.map(post => 
+                <Card className="mt-3 col-sm-1 col-md-9" key={post.keyPost}>
                     <CardHeader>
-                    <img src="" width="30px" className="img-fluid z-depth-1 rounded-circle" alt="Imagen usuario"></img>
+                    <img src={post.photo} width="30px" className="img-fluid z-depth-1 rounded-circle mr-3" alt="Imagen usuario"></img> {post.name} dice:
                     </CardHeader>
                     <CardBody>
-                        <p name="textPost" className="col-12"></p>
-                        <li className="list-inline-item pr-2 ml-auto"><a href="#" className="white-text" ><i className="far fa-trash-alt fa-xs icon"></i> Delete</a></li>
+                        <p name="textPost" className="col-12">{post.textPost}</p>
+                        <li className="list-inline-item pr-2 ml-auto"><a href="#" className="white-text" onClick={this.deletePost}><i className="far fa-trash-alt fa-xs icon"></i> Delete</a></li>
                         <li className="list-inline-item pr-2"><a href="#" className="white-text" ><i className="far fa-edit fa-xs icon"> </i> Edit</a></li>
-                        <li className="list-inline-item"><a href="#" className="white-text"><i className="far fa-thumbs-up"></i>  Like</a></li>
+                        <li className="list-inline-item"><a href="#" className="white-text"><i className="far fa-thumbs-up"></i> {post.likes}  Like</a></li>
                         <li className="list-inline-item"><a href="#" className="white-text"><i className="far fa-thumbs-down icon"></i>  Dislike</a></li>
                     </CardBody>
                 </Card>
+                )}
             </div>
         )
     }
